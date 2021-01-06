9252531888eef7bccb5bcc5a7adc18581fe9d73e
MalekBenJemia <malekbenjemia@gmail.com>
2021-01-06 18:54:22 -0500

    font and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -266,5 +266,11 @@ var auditTask = function(taskEl) {
     }
 };
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
+
 // load tasks for the first time
 loadTasks();
